9b6db27bab8ce4f4b83cab98384cd1aefa816c9c
jonathanimolesi <jonjonimolesi@gmail.com>
2024-01-08 23:37:05 -0300

    Parcial head html created

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
 
 
 def sobre(request):
